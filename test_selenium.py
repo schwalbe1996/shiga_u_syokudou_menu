2c9a747a0431a364d300c72eee667c14dba7ed7e
konya-ga-yamada <dannourasizuka@yahoo.co.jp>
2025-01-20 11:40:26 +0900

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+import requests
+import time
 
 options = Options()
 options.add_argument("--headless")
@@ -9,7 +16,7 @@ options.add_argument("--no-sandbox")
 # ポイント: 以下のように「executable_path」は不要
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.python.org/")
+driver.get("https://west2-univ.jp/sp/menu.php?t=657611")
 time.sleep(3)
 
 print("Page title is:", driver.title)
